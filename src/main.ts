e1760276559271b1e9f5f13da561720060361b1b
iukh <33911363+iukh@users.noreply.github.com>
2019-01-21 03:43:57 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,37 @@
-class NodeElement {
+interface INode {
   leftNode: NodeElement;
   rightNode: NodeElement;
   value: number;
-  constructor(Value: number) {
+}
+class NodeElement implements INode {
+  leftNode: NodeElement;
+  rightNode: NodeElement;
+  private _value: number;
+  constructor(value: number) {
     this.leftNode = null;
     this.rightNode = null;
-    this.value = Value;
+    this._value = value;
+  }
+  get value(): number {
+    return this._value;
+  }
+  set value(newValue: number) {
+    this._value = newValue;
   }
 }
 class binaryTree {
   rootNode: NodeElement;
-  constructor(){
+  constructor() {
     this.rootNode = null;
   }
-  addNode(Value: number): void {
+  addNode(value: number): void {
     if (this.rootNode == null) {
-      this.rootNode = new NodeElement(Value);
+      this.rootNode = new NodeElement(value);
       return;
     }
-    this.addNodeRecursion(this.rootNode, Value);
+    this.addNodeRecursion(this.rootNode, value);
   }
-  addNodeRecursion(currentNode: NodeElement, newValue: number): void{
+  private addNodeRecursion(currentNode: NodeElement, newValue: number): void{
     if (currentNode.value < newValue) {
       if (currentNode.rightNode == null) {
         currentNode.rightNode = new NodeElement(newValue);
@@ -42,74 +53,72 @@ class binaryTree {
       console.log("Tree is not created yet");
     } else {
       console.log("Root: " + this.rootNode.value)
-      this.printTreeT(this.rootNode, 1);
+      this.printTreeRecursion(this.rootNode, 1);
     }
   }
-  printTreeT(currentNode: NodeElement, level: number) {
+  private printTreeRecursion(currentNode: NodeElement, level: number) {
     if (currentNode.leftNode != null) {
       console.log("LB"+ '---'.repeat(level) + ">" + currentNode.leftNode.value );
-      this.printTreeT(currentNode.leftNode, level + 1);
+      this.printTreeRecursion(currentNode.leftNode, level + 1);
     }
     if (currentNode.rightNode != null) {
       console.log("RB"+ '---'.repeat(level) + ">" + currentNode.rightNode.value);
-      this.printTreeT(currentNode.rightNode, level + 1);
+      this.printTreeRecursion(currentNode.rightNode, level + 1);
     }
   }
   searchNode(value: number): NodeElement {
-    let flag: boolean = false;
     if (this.rootNode.value == value) {
       return this.rootNode;
     } else {
-        return this.searchNodeT(this.rootNode, value);
+      return this.searchNodeRecursion(this.rootNode, value);
     }
   }
-  searchNodeT(currentNode: NodeElement, value: number): NodeElement {
+  private searchNodeRecursion(currentNode: NodeElement, value: number): NodeElement {
     if (currentNode.value == value) {
       return currentNode;
     }
     if(value < currentNode.value) {
       if (currentNode.leftNode != null) {
-        return this.searchNodeT(currentNode.leftNode, value);
+        return this.searchNodeRecursion(currentNode.leftNode, value);
       } else return null;
     } else {
       if (currentNode.rightNode != null) {
-        return this.searchNodeT(currentNode.rightNode, value);
+        return this.searchNodeRecursion(currentNode.rightNode, value);
       } else return null;
     }
   }
   removeNode(value: number): void {
-     if (this.rootNode.value == value) {
-       this.rootNode = null;
-     }
-     let currentNode: NodeElement = this.rootNode;
-     let parentNode: NodeElement  = null;
-     while (currentNode.value != value) {
-       if (currentNode.value > value) {
-         parentNode = currentNode;
-         currentNode = currentNode.leftNode;
-       } else {
-         parentNode = currentNode;
-         currentNode = currentNode.rightNode;
-       }
-     }
-     if (currentNode.leftNode == null && currentNode.rightNode == null) {
-       if (parentNode.leftNode == currentNode ) {
-         parentNode.leftNode = null;
-       } else parentNode.rightNode = null;
-     } else if (currentNode.leftNode == null || currentNode.rightNode == null) {
-       if (currentNode.leftNode == null) {
-         if (parentNode.leftNode == currentNode ) {
-           parentNode.leftNode = currentNode.rightNode;
-         } else parentNode.rightNode = currentNode.rightNode;
-       } else {
-         if (parentNode.leftNode == currentNode ) {
-           parentNode.leftNode = currentNode.leftNode;
-         } else parentNode.rightNode = currentNode.leftNode;
-       }
-     }
-     if(currentNode.leftNode != null && currentNode.rightNode != null) {
-
-     }
+    if (this.rootNode.value == value) {
+      this.rootNode = null;
+    }
+    let currentNode: NodeElement = this.rootNode;
+    let parentNode: NodeElement  = null;
+    while (currentNode.value != value) {
+      if (currentNode.value > value) {
+        parentNode = currentNode;
+        currentNode = currentNode.leftNode;
+      } else {
+        parentNode = currentNode;
+        currentNode = currentNode.rightNode;
+      }
+    }
+    if (currentNode.leftNode == null && currentNode.rightNode == null) {
+      if (parentNode.leftNode == currentNode ) {
+        parentNode.leftNode = null;
+      } else parentNode.rightNode = null;
+    } else if (currentNode.leftNode == null || currentNode.rightNode == null) {
+      if (currentNode.leftNode == null) {
+        if (parentNode.leftNode == currentNode ) {
+          parentNode.leftNode = currentNode.rightNode;
+        } else parentNode.rightNode = currentNode.rightNode;
+      } else {
+        if (parentNode.leftNode == currentNode ) {
+          parentNode.leftNode = currentNode.leftNode;
+        } else parentNode.rightNode = currentNode.leftNode;
+      }
+    }
+    if(currentNode.leftNode != null && currentNode.rightNode != null) {
+    }
   }
 }
 
@@ -119,7 +128,7 @@ console.log("LB - Left Branch, RB - Right Branch");
 console.log("");
 let FirstTree: binaryTree = new binaryTree();
 for(let i = 0; i < nodeList.length; i++) {
-   FirstTree.addNode(nodeList[i]);
+  FirstTree.addNode(nodeList[i]);
 }
 FirstTree.printTree();
 FirstTree.addNode(6);
